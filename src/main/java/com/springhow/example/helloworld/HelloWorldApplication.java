4f4cc863351ab13d68efdbb8fbc4fc9d2c150adf
santoshahirwar <santoshahirwar@cdnsol.com>
2022-05-31 15:37:19 +0530

    change line in code

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-         return "Hello Auto Maven Task";
+         return "Hello Maven code Task";
 
     }
 
