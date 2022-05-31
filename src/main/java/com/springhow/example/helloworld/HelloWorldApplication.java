be7bdd541ead5e258461162a85d89a412a29bbde
aayanshahirwar <76963533+aayanshahirwar@users.noreply.github.com>
2022-05-31 13:16:17 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "HELLO AUTOMATIONS TASK WITH MAVEN";
+        return "HELLO MAVEN AUTOMATIONS TASK";
     }
 
 }
